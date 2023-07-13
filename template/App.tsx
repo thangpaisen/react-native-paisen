5c65e3d717e6ab5e797f848b26c778de64f566fd
thangpaisen <thangpaisen@gmail.com>
2023-07-13 16:47:05 +0700

    feat: toast message

--- template/App.tsx
+++ template/App.tsx
@@ -1,12 +1,14 @@
-import 'react-native-gesture-handler'
-import React from 'react'
-import { Provider } from 'react-redux'
 import ApplicationNavigator from '@/Navigators/Application'
 import store from '@/Store'
+import React from 'react'
+import 'react-native-gesture-handler'
+import Toast from 'react-native-toast-message'
+import { Provider } from 'react-redux'
 
 const App = () => (
   <Provider store={store}>
     <ApplicationNavigator />
+    <Toast />
   </Provider>
 )
 
