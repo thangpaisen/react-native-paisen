6fc08c0adebcc7e51bd03d6444e4914c3b67fa43
thangpaisen <thangpaisen@gmail.com>
2023-07-14 15:45:45 +0700

    add redux-persist

--- template/App.tsx
+++ template/App.tsx
@@ -1,14 +1,17 @@
 import ApplicationNavigator from '@/Navigators/Application'
-import store from '@/Store'
+import store, { persistor } from '@/Store'
 import React from 'react'
 import 'react-native-gesture-handler'
 import Toast from 'react-native-toast-message'
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
 
 const App = () => (
   <Provider store={store}>
-    <ApplicationNavigator />
-    <Toast />
+    <PersistGate persistor={persistor} loading={null}>
+      <ApplicationNavigator />
+      <Toast />
+    </PersistGate>
   </Provider>
 )
 
--- template/src/Store/index.ts
+++ template/src/Store/index.ts
@@ -5,11 +5,13 @@ import axios from 'axios'
 import axiosMiddleware from 'redux-axios-middleware'
 import { HOST } from '@/Services/endpoints'
 import get from 'lodash/get'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { persistStore, persistReducer } from 'redux-persist'
 
 const client = axios.create({
   //all axios can be used, shown in axios documentation
   baseURL: HOST,
-  responseType: 'json'
+  responseType: 'json',
   // params: { api_key: '1270f8b48ea8fdfeb112071f737ab3bb' }
 })
 
@@ -25,19 +27,16 @@ const apiMiddleware = {
             get(req, 'reduxSourceAction.payload.request.headers')
           )
           if (get(store.getState(), 'user.token')) {
-            headers.Authorization = `Token ${get(
-              store.getState(),
-              'user.token'
-            )}`
+            headers.Authorization = `Token ${get(store.getState(), 'user.token')}`
           }
           return {
             ...req,
             headers: {
-              common: headers
-            }
+              common: headers,
+            },
           }
-        }
-      }
+        },
+      },
     ],
     response: [
       {
@@ -46,16 +45,27 @@ const apiMiddleware = {
         },
         error: function (store: any, error: any) {
           return Promise.reject(error)
-        }
-      }
-    ]
-  }
+        },
+      },
+    ],
+  },
 }
 
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+  //   whitelist: ['user'], // only navigation will be persisted
+  //   blacklist: ['navigation'] // navigation will not be persisted
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
 let store = configureStore({
-  reducer: rootReducer,
-  middleware: [thunk, axiosMiddleware(client, apiMiddleware)]
+  reducer: persistedReducer,
+  middleware: [thunk, axiosMiddleware(client, apiMiddleware)],
 })
+
+export const persistor = persistStore(store)
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
