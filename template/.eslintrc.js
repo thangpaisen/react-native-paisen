f29f90c9410d262859cfcc1f77f7cf7a6617c6b4
noname <paisenzzz>
2025-05-20 16:15:28 +0700

    Add parserOptions to ESLint configuration

--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -1,6 +1,9 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  parserOptions: {
+    requireConfigFile: false,
+  },
   rules: {
     semi: 'off',
     'react-hooks/exhaustive-deps': 'off',
